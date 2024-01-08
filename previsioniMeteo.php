77e255369f7e0ce8e3f81d7b112b4c397e57665b
spar-git <stefano92lt@libero.it>
2024-01-08 18:40:40 +0100

    Update previsioniMeteo.php

--- previsioniMeteo.php
+++ previsioniMeteo.php
@@ -7,14 +7,40 @@ foreach ( file("meteo.xml") as $node ) {
 $doc = new DOMDocument();
 $doc->loadXML($xmlString);
     
-if ($doc->schemaValidate("meteo.xsd")) {
-    echo "<p>Il documento XML è valido secondo lo schema.</p>\n";
-} else {
+if (!$doc->schemaValidate("meteo.xsd")) {
     echo "<p>Errore: Il documento XML non è valido secondo lo schema.</p>\n";
 }
 $root = $doc->documentElement;
 $elements = $root->childNodes;
 $total_elements = $elements->length;
+
+$current_item=($total_elements-7);
+if (isset($_GET['elem']))
+    $current_item = $_GET['elem'];
+
+$giorno = $root->getElementsByTagName('giorno')->item($current_item);
+$condizione = $giorno->getElementsByTagName('condizione')->item(0)->nodeValue;
+if ($condizione == 'Soleggiato'){
+    $img_src= "solegg.jpg";
+    $img_alt= "Soleggiato";
+}
+if ($condizione == 'Parzialmente nuvoloso'){
+    $img_src= "parz_nuvol.jpg";
+    $img_alt= "Parzialmente nuvoloso";
+}
+if ($condizione == 'Pioggia e schiarite'){
+    $img_src= "piog_schiar.jpg";
+    $img_alt= "Pioggia e schiarite";
+}
+if ($condizione == 'Pioggia'){
+    $img_src= "piogg.jpg";
+    $img_alt= "Pioggia";
+}
+if ($condizione == 'Temporale'){
+    $img_src= "tempor.jpg";
+    $img_alt= "Temporale";
+}
+
 ?>
 
 <?xml version="1.0" encoding="ISO-8859-1"?>
@@ -30,14 +56,17 @@ $total_elements = $elements->length;
 <body>
 
     <div class="town-container">
-    <img src="solegg.jpg" alt="Soleggiato">
+    <?php echo "<img src=\"$img_src\" alt=\"img_alt\">"; ?>
         <p class="town">METEO Latina</p>
     </div>
 
 <table class="date-table">
   <tr>
     <?php for ($i=($total_elements-7); $i<$total_elements; $i++) {
-    echo "<td>";
+        if ($i==$current_item)
+            echo "<td style=\"background-color: blue;\">";
+        else
+            echo "<td>";
         $giorno = $root->getElementsByTagName('giorno')->item($i);
         $data = $giorno->getAttribute('data');
         echo "<a href=\"previsioniMeteo.php?elem=$i\">$data</a>";
@@ -49,7 +78,7 @@ $total_elements = $elements->length;
     <div class="box3">
     <?php
 
-    
+    $giorno = $root->getElementsByTagName('giorno')->item($current_item);
     $data = $giorno->getAttribute('data');
     echo "<h2>Previsioni per il giorno $data</h2>";
     $condizione = $giorno->getElementsByTagName('condizione')->item(0)->nodeValue;
@@ -65,7 +94,7 @@ $total_elements = $elements->length;
     $precipitazioni = $giorno->getElementsByTagName('precipitazioni')->item(0);
     $probabilitaPrecipitazioni = $precipitazioni->getAttribute('probabilita');
     $intensitaPrecipitazioni = $precipitazioni->getAttribute('intensita');
-    echo "<p>Precipitazioni: Probabilità $probabilitaPrecipitazioni%, Intensità $intensitaPrecipitazioni mm/h</p>";
+    echo "<p>Precipitazioni: Probabilità $probabilitaPrecipitazioni%, Intensità $intensitaPrecipitazioni mm</p>";
     $luna = $giorno->getElementsByTagName('luna')->item(0);
     $faseLuna = $luna->getElementsByTagName('fase')->item(0)->nodeValue;
     $illuminazioneLuna = $luna->getElementsByTagName('illuminazione')->item(0)->nodeValue;
