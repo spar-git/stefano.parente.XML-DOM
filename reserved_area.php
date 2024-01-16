ad280a429b807a4d60d097458c8d77844a68d5cc
spar-git <stefano92lt@libero.it>
2024-01-16 12:02:43 +0100

    Update reserved_area.php

--- reserved_area.php
+++ reserved_area.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+//Impedisce l'accesso tramite url
 if (isset($_SESSION['accesso_consentito'])) {
     if ($_SESSION['accesso_consentito']==false) {
         header("Location: admin_login.php");
@@ -14,21 +15,25 @@ if (isset($_SESSION['accesso_consentito'])) {
 $message="";
 
 $xmlString = "";
+// Itera attraverso ogni riga del file "meteo.xml" rimuovendo spazi vuoti
 foreach ( file("meteo.xml") as $node ) {
 	$xmlString .= trim($node);
 }
 
+//Crea un nuovo oggetto DOMDocument e carica il contenuto XML nella DOMDocument
 $doc = new DOMDocument();
 if (!$doc->loadXML($xmlString)) {
     die ("Errore parsing del documento\n");
 }
-    
+
+// Verifica se il documento XML non è valido secondo lo schema "meteo.xsd"
 if (!$doc->schemaValidate("meteo.xsd")) {
     echo "<p>Errore: Il documento XML non è valido secondo lo schema.</p>\n";
 }
 
 $root = $doc->documentElement;
 
+//Riceve tutti i dati inseriti dall'utente per comporre un nuovo elemento "giorno"
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['invia'])) {
         $data = $_POST["data"];
@@ -47,14 +52,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $newGiorno->setAttribute("data", "$data");
 
         $newCondizione = $doc->createElement("condizione", "$condizione");
+        
         $newTemperatura = $doc->createElement("temperatura", "$temperatura");
+
         $newUmidita = $doc->createElement("umidita", "$umidita");
+
         $newVento= $doc->createElement("vento");
         $newVento->setAttribute("velocita", "$velocita");
         $newVento->setAttribute("direzione", "$direzione");
+
         $newPrecipitazioni = $doc->createElement("precipitazioni");
         $newPrecipitazioni->setAttribute("probabilita", "$probabilita");
         $newPrecipitazioni->setAttribute("intensita", "$intensita");
+
         $newLuna = $doc->createElement("luna");
         $newFase = $doc->createElement("fase", "$fase");
         $newIlluminazione = $doc->createElement("illuminazione", "$illuminazione");
@@ -72,8 +82,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $newGiorno->appendChild($newLuna);
 
         $root->appendChild($newGiorno);
-
+        
+        //Percorso completo del file XML, incluso il nome del file ("meteo.xml")
         $path=dirname(__FILE__) . "/meteo.xml";
+        // Salva il documento XML nell'indirizzo specificato da $path
         $doc->save($path);  
 
         $message="Le previsioni per il giorno " . $data . "<br>sono state caricate correttamente nel doc XML";
@@ -81,6 +93,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $elements = $root->childNodes;
     $total_elements = $elements->length;
+
+    //Il controllo "$total_elements>7" può sembrare ridondante in quanto già effettuato nella riga 226 (se gli elementi sono <= 7 non compare il tasto "elimina"). 
+    //E' servito ripeterlo qui in quanto, in caso di refresh della pagina, l'eliminazione si reiterava sugli elementi successivi, ignorando il controllo della riga 226
+    //e di conseguenza causando problemi alla corretta visualizzazione delle date nel menù in "previsioniMeteo.php"
     if (isset($_POST['elimina'])&&$total_elements>7) {
         $ultimo = $root->lastChild;
         $root->removeChild($ultimo);
@@ -111,6 +127,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </div>
 <div class="container">
     <?php 
+        //messaggio di avvenuto inserimento
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST['invia'])) {
                 echo '<h4 style="color:green; text-align: center;">' . $message . '</h4>';
@@ -120,6 +137,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
     <form class="reserved-area" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <h2 style>Inserisci previsioni meteo</h2>
+    
+    <!-- I seguenti campi di input implementano regole restrittive affinchè non ci siano ambiguità ed errori di validazione del doc XML-->
+    
     <p>Data (gg-mm-aaaa):</p>
     <input type="text" id="data" name="data" pattern="\d{2}-\d{2}-\d{4}" required>
 
@@ -187,6 +207,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <div class="container">
     <?php 
+        //Messaggio di avvenuta eliminazione
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST['elimina'])) {
                 echo '<h4 style="color:green; text-align: center;">' . $message . '</h4>';
@@ -201,6 +222,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php 
         $elements = $root->childNodes;
         $total_elements = $elements->length;
+        //Se nel doc XML ci sono meno di 8 elementi, non è possibile eliminare altri elementi, causa problemi alla corretta visualizzazione delle 7 date nel menù in "previsioniMeteo.php"
         if ($total_elements<=7)
             echo '<p style="color:red;">Devi aggiungere dei giorni prima di poterli eliminare <br>(numero di giorni minimo = 7)</p>';
         else
